cda8c7c98fd7447899bd0bdd43463bf67dca8614
Beatriz Paiva <beeepaiva@gmail.com>
2025-02-18 20:32:48 -0300

    adding map

--- aula02-react/src/App.jsx
+++ aula02-react/src/App.jsx
@@ -6,19 +6,29 @@ function App() {
 
 const curso = "tads";
 const number = 1;
-const arrXPTO = [1,2,3,4,5];
+const arrNumerico = [1,2,3,4,5];
+const arrAulas = [{id: 1, nome:"aula 1"}, {id:2, nome: "aula 2"}]
 const objCurso = {nome: "BCC", detalhes: "Ciência da computação"}
 
   return (
   <div className='App'>
   <Header />
+  <ul>
+  {arrNumerico.map((numero) => {return<li>{numero} <button>Clique</button></li>}) }
+  </ul>
+  <ul>
+    {arrAulas.map((aula)=> {return <li>{aula.id} - {aula.nome}</li>})}
+  </ul>
+  {/** 
   <h1>{objCurso.nome}</h1>
   <h2>{objCurso.detalhes}</h2>
   <Botao textDisplay="Botao 1" isDisabled />
   <Botao textDisplay="Botao 2" color="blue"/>
   <Botao textDisplay="Botao 3" color="red"/>
   <h1>botao</h1>
-  <Botao textDisplay="Botao 4" isDisabled/>
+  <Botao textDisplay="Botao 4" isDisabled/>*/}
+
+
   </div>
   )
 }
--- aula02-react/src/componentes/Header.jsx
+++ aula02-react/src/componentes/Header.jsx
@@ -2,15 +2,32 @@ import React from "react";
 import "./Header.css";
 import { Botao } from "./Botao";
 
+function handleOnClick(){
+  alert("Função chamada!!");
+}
+
+function handleOnChange(e){
+  console.log(e.target.value);
+}
+
+function BotaoHandleClick({textDisplay, ...props}){
+  const msg = textDisplay + ' - ' + props.msgAlerta;
+  return (
+    <button onClick={()=>{alert(textDisplay + ' - ' + textDisplay)} } >{textDisplay}</button>
+  )
+}
+
 export function Header() {
     return(
   <div>
     <header className="header">
       <h1 className="heading">Aula 02 - Componentes</h1>
       <nav className="nav">
-        <button className="nav-button">Página Inicial</button>
-        <button className="nav-button">Curso</button>
-        <button className="nav-button">Professores</button>
+        <input onChange={handleOnChange}></input>
+        <button className="nav-button" onClick={()=>{console.log("Botao clicado")}}>Página Inicial</button>
+        <button className="nav-button" onClick={handleOnClick}>Curso</button>
+        <button className="nav-button" onMouseLeave={()=>{console.log("On Mouse Leave")}}>Professores</button>
+        <BotaoHandleClick textDisplay="Botao 1" msgAlerta="Mensagem passada"/>
         <Botao color="pink" textDisplay="xpto"/>
       </nav>
     </header>
